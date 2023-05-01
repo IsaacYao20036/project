74952cdc1757010ede1a101531e22e3ce25e93b0
Isaac Yao <20036@burnside.school.nz>
2023-05-01 12:52:52 +1200

    initial commit

---
+++ routes.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
+
+@app.route("/our_products")
+def our_products():
+    conn = sqlite3.connect("project.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Flavour")
+    flavours = cur.fetchall()
+    return render_template("our_products.html", flavours = flavours)
+
+
+@app.route("/order_&_delivery")
+def order_and_delivery():
+    return render_template("order_&_delivery.html")
