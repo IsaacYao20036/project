2b5dad428d2de59af1801ab275d2a50af79d603c
IsaacYao20036 <20036@burnside.school.nz>
2023-06-24 14:38:31 +1200

    Added number field in orders page & span elements

--- static/script.js
+++ static/script.js
@@ -1,8 +1,21 @@
 var selectionCounter = 0
-function cloneSelect() {
+function cloneFunction() {
 
   var br = document.createElement("br");
   document.getElementById("divContainer").appendChild(br)
+
+  var input = document.getElementById("quantity")
+  var clone = input.cloneNode(true)
+  var name = input.getAttribute("name") + selectionCounter++
+  clone.id = name
+  clone.setAttribute("name", name)
+  document.getElementById("divContainer").appendChild(clone).value = ""
+
+  var span = document.createElement("span");
+  var node = document.createTextNode(" x \u00A0");
+  span.appendChild(node);
+  var element = document.getElementById("divContainer");
+  element.appendChild(span);
 
   var select = document.getElementById("flavours")
   var clone = select.cloneNode(true)
@@ -10,6 +23,12 @@ function cloneSelect() {
   clone.id = name
   clone.setAttribute("name", name)
   document.getElementById("divContainer").appendChild(clone)
+
+  var span = document.createElement("span");
+  var node = document.createTextNode("\u00A0 in \u00A0");
+  span.appendChild(node);
+  var element = document.getElementById("divContainer");
+  element.appendChild(span);
 
   var select = document.getElementById("containers")
   var clone = select.cloneNode(true)
@@ -20,4 +39,4 @@ function cloneSelect() {
  
 }
 
-/* This code is from https://stackoverflow.com/questions/34471016/adding-new-select-tag-by-clicking-on-the-button */
+/* A majority of this code is from https://stackoverflow.com/questions/34471016/adding-new-select-tag-by-clicking-on-the-button */
