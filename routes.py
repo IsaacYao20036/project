854525e652aa318f68cd463e4a417c2d53de5b13
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-08-01 10:27:21 +1200

    working on 404 page

--- routes.py
+++ routes.py
@@ -47,5 +47,9 @@ def order_and_delivery():
                            containers=containers)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
