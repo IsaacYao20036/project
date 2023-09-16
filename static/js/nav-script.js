13409f17c15ee61193358a5e724717264daf595a
IsaacYao20036 <20036@burnside.school.nz>
2023-09-16 16:36:21 +1200

    fontchange for dropdwnlinks&appear longr afterhovr (more usable drowdown links)

--- static/js/nav-script.js
+++ static/js/nav-script.js
@@ -1,18 +1,29 @@
-/* Based on the page the user is on, this script changes the corresponding nav hyperlink to a different font */
+// Based on the page the user is on, this script changes the corresponding nav hyperlink to a different font
 
-let path = location.pathname; /* Gets URL of current page, stores in path */
-let linkID;
+let path = location.pathname; // Gets URL of current page, stores in path
+let linkID, linkID_2; // linkID_2 for links in dropdown
 
-/* Assigns linkID based on path */
+// Assigns linkID based on path
 if (path == "/") {
     linkID = "home"
 } else if (path == "/about_us") {
     linkID = "about_us"
-} else if (path == "/our_products") {
+} else if (path == "/flavours") {
     linkID = "our_products"
+    linkID_2 = "flavours"
+} else if (path == "/toppings") {
+    linkID = "our_products"
+    linkID_2 = "toppings"
+} else if (path == "/containers") {
+    linkID = "our_products"
+    linkID_2 = "containers"
 } else if (path == "/order_&_delivery") {
     linkID = "order_&_delivery"
 }
 
-/* Changes nav link to different font with same id in linkID */
-document.getElementById(linkID).style.fontFamily = "Bagel Fat One", fantasy ;
+// Changes nav link with the same id in linkID/linkID_2 to a different font
+document.getElementById(linkID).style.fontFamily = "Bagel Fat One, fantasy";
+
+if (linkID_2 != undefined) {
+    document.getElementById(linkID_2).style.fontFamily = "Bagel Fat One, fantasy";
+}
